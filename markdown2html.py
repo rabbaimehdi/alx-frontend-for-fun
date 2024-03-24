e05fbc7faa134cd88a619655e087a5c378a66e0a
rabbaimehdi <48589990+rabbaimehdi@users.noreply.github.com>
2024-03-24 22:31:57 +0000

    add corrections for task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+''' Write a script markdown2html.py that takes an argument 2 strings:
+    First argument is the name of the Markdown file
+    Second argument is the output file name
+'''
 
 import sys
 from os.path import exists
 
 if __name__ == '__main__':
     if (len(sys.argv) < 3):
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         exit(1)
 
     elif (not exists(sys.argv[1])):
