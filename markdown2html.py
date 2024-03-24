cd19a21ac47de5a958756b44e9eb2aab39d3ebdc
rabbaimehdi <48589990+rabbaimehdi@users.noreply.github.com>
2024-03-24 22:30:16 +0000

    add corrections for task 0

--- markdown2html.py
+++ markdown2html.py
@@ -3,13 +3,13 @@
 import sys
 from os.path import exists
 
-if (len(sys.argv) < 3):
-    print("Usage: ./markdown2html.py README.md README.html")
-    exit(1)
+if __name__ == '__main__':
+    if (len(sys.argv) < 3):
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
 
-elif (not exists(sys.argv[1])):
-    print(f"Missing {sys.argv[1]}")
-    exit(1)
+    elif (not exists(sys.argv[1])):
+        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        exit(1)
 
-else:
     exit(0)
